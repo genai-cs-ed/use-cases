59c562e22d643dd376f8ea0688e7f06b37d7bfe9
Kate Holdener <kate.holdener@gmail.com>
2026-02-03 14:56:26 -0600

    adjusted vite config for production build

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/ai-use-cases/',
       server: {
         port: 3000,
         host: '0.0.0.0',
